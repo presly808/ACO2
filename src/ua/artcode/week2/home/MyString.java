8a0ad7f3321de9303e237f54db8b55fbb0cc1b20
presly808 <presly808@gmail.com>
2014-10-20 12:51:14 +0300

    week2 home

---
+++ src/ua/artcode/week2/home/MyString.java
@@ -0,0 +1,115 @@
+package ua.artcode.week2.home;
+
+/**
+ * TODO Implement all methods, MyString must be immutable
+ * String docs  http://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+ */
+public class MyString {
+
+
+    public MyString(){
+
+    }
+
+    public MyString(char[] mas){
+
+    }
+
+    /*
+    * String docs
+    * */
+    public char charAt(int index){
+        return ' ';
+    }
+
+    /*
+    * Equals with other String
+    * */
+    public boolean eq(MyString other){
+        return false;
+    }
+
+    /*
+    * Concat to Strings
+    * */
+    public MyString concat(MyString myString){
+        return null;
+    }
+
+
+    /*
+    * Return split MyString by indexes
+    * */
+    public MyString substring(int start, int end){
+        return null;
+    }
+
+    /*
+    * True if this MyString contains target
+    * */
+    public boolean contains(MyString target){
+        return false;
+    }
+
+    /*
+    * See String documentation
+    * */
+    public MyString trim(){
+        return null;
+    }
+
+
+    /*
+    * See String documentation
+    * */
+    public int indexOf(MyString myString){
+        return -1;
+    }
+
+    /*
+    * example: some text for -> sometextfor
+    *
+    * */
+    public MyString deleteAllSpaces(){
+        return null;
+    }
+
+
+    /*
+    * example abcde - > edcba
+    *
+    * */
+    public MyString reverse(){
+        return null;
+    }
+
+    /*
+    * Replaces each substring of this string that matches the given
+    * */
+    public MyString replace(MyString target, MyString replacement){
+        return null;
+    }
+
+    /*
+    * Show MyString to console
+    * */
+    public void print(){
+
+    }
+
+    /*
+    * Converts MyString  to String
+    * */
+    public String convert(){
+        return null;
+    }
+
+    /*
+    * Sort MyString
+    * example bxdzal -> abdlxz
+    * */
+    public MyString sort(){
+        return null;
+    }
+
+}
